dd13bc2bf0fdacadcbf93f43d9e0459b31434c5f
Sky DeGrate <skydegrate@gmail.com>
2022-08-24 13:53:34 -0400

    added infinite scrolling to song cards

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -30,7 +30,7 @@ function SongCard({ url, user, onDeleteSong }) {
 				visual={false}
 				hideRelated={true}
 			/>
-			<button onClick={onDeleteClick}>remove</button>
+			<span class="tag is-dark" style={{float: "right", marginBottom: ""}} onClick={onDeleteClick}>remove</span>
 		</div>
 	)
 }
